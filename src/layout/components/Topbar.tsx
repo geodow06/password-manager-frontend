1f7c2bd57117576ef670b2a92737c471ab73fea5
George <georgedowne06@gmail.com>
2021-11-10 13:35:57 +0000

    Updated the styling

--- src/layout/components/Topbar.tsx
+++ src/layout/components/Topbar.tsx
@@ -3,7 +3,7 @@ import { Component } from "react";
 class Topbar extends Component {
     render() {
         return(
-            <div className="tobar">
+            <div className="topbar">
                 Topbar
             </div>
         );
