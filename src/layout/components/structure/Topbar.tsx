53d20bdee53b9dbfbba47c0cc0986e0027da77b2
George <georgedowne06@gmail.com>
2022-01-16 13:19:35 +0000

    Added Brand to topbar

--- src/layout/components/structure/Topbar.tsx
+++ src/layout/components/structure/Topbar.tsx
@@ -1,10 +1,13 @@
 import { Component } from "react";
+import Brand from "../content/Brand";
 
 class Topbar extends Component {
     render() {
         return(
             <div className="topbar">
-                Topbar
+                <div className="topbar-hold">
+                    <Brand/>
+                </div>
             </div>
         );
     }
