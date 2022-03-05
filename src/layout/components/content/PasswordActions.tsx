f8f048acc1bcbdced13c7cf5a6fc3b3978c8c9da
George <georgedowne06@gmail.com>
2022-03-05 13:41:20 +0000

    Added action menu to perform actions on selected accounts

--- src/layout/components/content/PasswordActions.tsx
+++ src/layout/components/content/PasswordActions.tsx
@@ -1,13 +1,66 @@
 import { useCustomSelector } from "redux/reduxTypes";
+import { Button, Menu, MenuItem } from "@material-ui/core"
+import React from "react";
 
 const PasswordActions = () => {
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
     const selected = useCustomSelector(state => state.selected);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const close = () => {
+        setAnchorEl(null);
+    };
+
+    const handleDelete = () => {
+        selected.forEach(account => console.log(`Deleting account ${account.accountId}`));
+        close()
+    };
+
+    const handleCopy = () => {
+        console.log(`Copying password for account ${selected[0].accountId}`);
+        copy(selected[0].value);
+        close()
+    };
+
+    const handleEdit = () => {
+        console.log(`Editing account ${selected[0].accountId}`);
+        close()
+    };
+
+    const copy = (text: string) => {
+        navigator.clipboard.writeText(text);
+    };
+
+    const multipleSelected = selected.length > 1;
+
     return (
         <div>
-            {selected.length === 0 && <div>none selected</div>}
-            {selected.map((account, index) => (
-                <div key={index}>{account.accountName}</div>
-            ))}
+            {selected.length !== 0 && 
+            <div>
+                    <div>{selected.length} items selected</div>
+                    <Button onClick={handleClick}>
+                        Actions
+                    </Button>
+                    <Menu
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={close}
+                    >
+                        <MenuItem disabled={multipleSelected} onClick={handleEdit} disableRipple>
+                            Edit
+                        </MenuItem>
+                        <MenuItem disabled={multipleSelected} onClick={handleCopy} disableRipple>
+                            Copy Password
+                        </MenuItem>
+                        <MenuItem disabled={!multipleSelected} onClick={handleDelete} disableRipple>
+                            Delete
+                        </MenuItem>
+                    </Menu>
+            </div>
+            }
         </div>
     );
 };
